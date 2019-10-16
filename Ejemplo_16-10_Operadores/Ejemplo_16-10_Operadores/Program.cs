f896f6c39a4694cb995138d13908bc99386a5292
Douglas Warner Jurado Peña <douglaswarner.jp@gmail.com>
2019-10-16 14:26:59 +0200

    Programacion 19-20

---
+++ Ejemplo_16-10_Operadores/Ejemplo_16-10_Operadores/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo_16_10_Operadores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x = 10;
+            int y = 10;
+            byte b = 254;
+            char letra = 'A';
+            
+            Console.WriteLine("El valor de x es {0} y con y++ es {1}. Asigna despues.", x, y++);
+            y = 10;
+
+            Console.WriteLine("El valor de x es {0} y con ++y es {1}. Asigna antes.", x, ++y);
+            
+            // checked          CHECKED hace que compruebe si desborda la variable, y saltaria excepcion no controlada.
+            //
+                Console.WriteLine("\nPor defecto la comprobacion del desbordamiento de variables es unchecked. Esto es lo que sucede al incrementar.\n\nEl valor de b es {0}", b);
+                Console.WriteLine("El valor de b es {0}", ++b);
+                Console.WriteLine("El valor de b es {0}", ++b);
+            //}
+
+            Console.WriteLine("\n\n El ASCII de la letra {0} es {1}", letra, (int)letra);
+            Console.WriteLine("\n\n El entero de 122 es la letra {0}", (char)122);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Tareas/R3_Jurado_Douglas/Ejercicio5/Ejercicio5/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio5
+{
+    class CalcularAreas
+    {
+        static void Main(string[] args)
+        {
+            string teclaPulsada = string.Empty;
+            int posArr = 10;
+            int posIzq = 5;
+
+            do
+            {
+                Console.Clear();
+                PintarMenu(posArr, posIzq);
+                Console.CursorVisible = true;
+                teclaPulsada = Console.ReadLine();
+
+                switch (teclaPulsada)
+                {
+                    case "1":
+                        Console.Clear();
+                        CalcularAreaTriangulo();
+                        Console.ReadLine();
+                        break;
+                    case "2":
+                        Console.Clear();
+                        CalcularAreaCuadrado();
+                        Console.ReadLine();
+                        break;
+                    case "3":
+                        Console.Clear();
+                        CalcularAreaRectangulo();
+                        Console.ReadLine();
+                        break;
+                    case "0":
+                        
+                        Console.WriteLine("Saliendo...");
+                        break;
+                    default:
+                        MensajeError(posArr, posIzq, "Porfavor, selecciona una de las opciones.");
+                        Console.CursorVisible = false;
+                        Console.ReadLine();
+                        Console.ResetColor();
+                        break;
+                }
+            } while (teclaPulsada != "0");
+
+            Console.ReadLine();
+        }
+
+
+        static void PintarMenu(int posArr, int posIzq)
+        {
+            /*if (posArr >= Console.WindowHeight || posIzq >= Console.WindowWidth)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Superado los limites para pintar el menu");
+                return;
+            }*/
+
+            Console.CursorTop = posArr;
+            Console.CursorLeft = posIzq;
+
+            Console.WriteLine("     Menu principal");
+            Console.CursorLeft = posIzq;
+            Console.WriteLine("".PadLeft(30,'─'));
+            Console.CursorLeft = posIzq;
+            Console.WriteLine(" 1. Cálcular Área de un triangulo");
+            Console.CursorLeft = posIzq;
+            Console.WriteLine(" 2. Cálcular Área de un cuadrado");
+            Console.CursorLeft = posIzq;
+            Console.WriteLine(" 3. Cálcular Área de un rectángulo");
+            Console.CursorLeft = posIzq;
+            Console.WriteLine(" 0. Salir");
+            Console.CursorLeft = posIzq;
+            Console.Write("       Elige una opción: ");
+        }
+
+        static void MensajeError(int posArr, int posIzq, string mensaje)
+        {
+            Console.SetCursorPosition(posIzq + 45, posArr + 5);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(mensaje);
+        }
+
+        static void CalcularAreaTriangulo()
+        {
+            Console.WriteLine("Hola, voy a calcular el area de un triangulo");
+        }
+
+        static void CalcularAreaCuadrado()
+        {
+            Console.WriteLine("Hola, voy a calcular el area de un cuadrado");
+        }
+
+        static void CalcularAreaRectangulo()
+        {
+            Console.WriteLine("Hola, voy a calcular el area de un rectangulo");
+        }
+    }
+}
