b548f04f4fac97dfeb910835d7de338e9790a9f3
Douglas Warner Jurado Peña <douglaswarner.jp@gmail.com>
2019-10-31 09:15:46 +0100

    Ejemplo Excepciones y Metodo

---
+++ Ejemplo_31-10_Excepciones/Ejemplo_31-10_Excepciones/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo_31_10_Excepciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numerador = 0;
+            int denominador = 0;
+            double resultado = 0.0;
+
+            try
+            {
+                Console.WriteLine(" Se cálcular una división muy dificil ");
+                Console.Write(" Dime el numerador: ");
+                numerador = int.Parse(Console.ReadLine());
+                Console.Write("\n Dime el denominador: ");
+                denominador = int.Parse(Console.ReadLine());
+
+                resultado = numerador / denominador;
+
+                Console.WriteLine("\n\n     El resultado de este pedaso de división entre esto {0} y esto {1} es esto: {2}",
+                                    numerador, denominador, resultado);
+            }
+            catch (DivideByZeroException dEx)
+            {
+                Console.WriteLine(dEx.Message + dEx.TargetSite);
+            }
+            catch (FormatException fEx)
+            {
+                Console.WriteLine(fEx.Message);
+                return;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);                
+            }
+            finally
+            {
+                Console.WriteLine("\n     Ere umakina aciendo divisione");
+                Console.ReadLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Ejemplo_31-10_Metodos/Ejemplo_31-10_Metodos/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo_31_10_Metodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ValorAbsoluto(-9000));
+
+            Console.ReadLine();
+        }
+
+        static int ValorAbsoluto(int num)
+        {
+            return (num < 0) ? -(num) : num;
+        }
+        static decimal ValorAbsoluto(decimal num)
+        {
+            return (num < 0) ? -(num) : num;
+        }
+        static double ValorAbsoluto(double num)
+        {
+            return (num < 0) ? -(num) : num;
+        }
+        static float ValorAbsoluto(float num)
+        {
+            return (num < 0) ? -(num) : num;
+        }
+        static long ValorAbsoluto(long num)
+        {
+            return (num < 0) ? -(num) : num;
+        }
+        static sbyte ValorAbsoluto(sbyte num)
+        {
+            return (num < 0) ? (sbyte)-(num) : (sbyte)num;
+        }
+        static short ValorAbsoluto(short num)
+        {
+            return (num < 0) ? (short)-(num) : (short)num;
+        }
+    }
+}
