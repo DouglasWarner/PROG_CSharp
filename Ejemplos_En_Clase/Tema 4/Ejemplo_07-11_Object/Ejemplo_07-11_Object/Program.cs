13c7c40b35b67479be0090f7fff17573e0e19025
Douglas Warner Jurado Peña <douglaswarner.jp@gmail.com>
2019-11-07 09:15:21 +0100

    Ejemplo Enum y Object

---
+++ Ejemplos_En_Clase/Tema 4/Ejemplo_07-11_Object/Ejemplo_07-11_Object/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo_07_11_Object
+{
+    class Program
+    {
+        static int numero = 3;
+
+        static void Main(string[] args)
+        {
+            //  object o;
+            //      Métodos estaticos de herencia de OBJECT
+            //  o.Equals
+            //  o.ToString
+            //  o.GetType
+            //  o.GetHashCode
+
+            object e = new EventArgs();
+            miClass miclase = new miClass();
+
+            Console.WriteLine(e.GetType());
+            Console.WriteLine(e);
+            Console.WriteLine(e.Equals(miclase));
+
+            Console.ReadLine();
+        }
+    }
+
+    class miClass
+    { }
+}
---
+++ Ejemplos_En_Clase/Tema 4/Ejemplo_07-11_TiposDefinidoPorElUsuario/Ejemplo_07-11_TiposDefinidoPorElUsuario/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo_07_11_TiposDefinidoPorElUsuario
+{
+    class Program
+    {
+        enum Dedo
+        {
+            Menique,
+            Anular,
+            Medio,
+            Indice,
+            Pulgar
+        };
+
+        struct Mascota
+        {
+
+        };
+
+        static void Main(string[] args)
+        {
+            Dedo dedoMalito = new Dedo();
+            Random rnd = new Random();
+
+            dedoMalito = Dedo.Pulgar;
+
+            Console.WriteLine("Mi dedo malito es {0}", (int)dedoMalito);
+
+            Console.WriteLine("     Lista de dedos");
+            Console.WriteLine("--------------------------------------");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine((Dedo)i);
+            }
+            Console.WriteLine("-----------------------------");
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.ForegroundColor = (ConsoleColor)rnd.Next((int)ConsoleColor.Yellow);
+                Console.WriteLine("Hola Caracola");
+            }
+            Console.ResetColor();
+            Console.WriteLine("----------------------------");
+
+            for (ConsoleColor i = ConsoleColor.Black; i <= ConsoleColor.Yellow; i++)
+            {
+                Console.ForegroundColor = i;
+                Console.Write("* ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
