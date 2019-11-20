99da842527c80e840b2adc816b95927bb1936111
Douglas Warner Jurado Peña <douglaswarner.jp@gmail>
2019-11-20 14:44:02 +0100

    Ejercicio 6

---
+++ Ejemplo_GestionNaipes/Ejemplo_GestionNaipes/GestionBaraja.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo_GestionNaipes
+{
+    class GestionBaraja
+    {
+        private Random _rnd = new Random();
+        const int TAMANOBARAJA = 1;
+        public enum Palos { Oro, Espada, Copas, Basto }
+        public enum Valor { Uno = 1, Dos, Tres, Cuatro, Cinco, Seis, Siete, Sota = 10, Caballo, Rey }
+
+        public struct Naipe
+        {
+            Valor valor;
+            Palos palo;
+
+            public Naipe(Palos p, Valor v)
+            {
+                valor = v;
+                palo = p;
+            }
+
+            public void VerNaipe()
+            {
+                Console.Write(" {0} de {1}, ", valor, palo);
+            }
+        }
+
+        int nDatos = 0;
+
+        Naipe[] Baraja = new Naipe[100];
+        //Naipe[] Baraja = new Naipe[TAMANOBARAJA];
+
+        public bool Anadir(Naipe naipe)
+        {
+            if (nDatos == Baraja.Length)
+                return false;
+            Baraja[nDatos++] = naipe;
+            return true;
+        }
+
+        // True lo muestra con salto de linea, False lo muestra en la misma linea.
+        public void VerBaraja(bool formato)
+        {
+            if (formato)
+            {
+                for (int i = 0; i < nDatos; i++)
+                {
+                    Baraja[i].VerNaipe();
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                for (int i = 0; i < nDatos; i++)
+                {
+                    Baraja[i].VerNaipe();
+                }
+            }
+        }
+
+        public bool LlenarBaraja(int cuantos)
+        {
+            int valor;
+            int palo;
+
+            for (int i = 0; i < cuantos; i++)
+            {
+                valor = _rnd.Next(1, Enum.GetValues(typeof(GestionBaraja.Valor)).Length);
+                if (valor == 8 || valor == 9)
+                    valor = (int)Valor.Sota;
+                palo = _rnd.Next((int)Palos.Basto);
+
+                Anadir(new Naipe((Palos)palo, (Valor)valor));
+            }
+
+            return true;
+        }
+
+        public void Inicializar()
+        {
+            nDatos = 0;
+        }
+    }
+}
---
+++ Ejercicio6/Ejercicio6/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int X = 20;
+            const int Y = 20;
+            int[,] array2D = new int[X, Y];
+
+            Console.WriteLine("Esta aplicación iniciliza y muestra un array de 2 dimensiones. Y la copia a otra array.");
+            Console.WriteLine();
+
+
+            Console.ReadLine();
+        }
+
+        static void InicializarArray(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = 0;
+                }
+            }
+        }
+
+        static void MostrarArray(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i,j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static bool ArrayAlea(int[,] array)
+        {
+            Random rnd = new Random();
+            int valorMaximo = 50;
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = rnd.Next(valorMaximo);
+                }
+            }
+            return true;
+        }
+
+        static bool CopiarArray(int[,] arrayOriginal, int[,] arrayDestino)
+        {
+            for (int i = 0; i < arrayOriginal.GetLength(0); i++)
+            {
+                for (int j = 0; j < arrayOriginal.GetLength(1); j++)
+                {
+                    arrayDestino[i, j] = arrayOriginal[i, j];
+                }
+            }
+            return true;
+        }
+    }
+}
