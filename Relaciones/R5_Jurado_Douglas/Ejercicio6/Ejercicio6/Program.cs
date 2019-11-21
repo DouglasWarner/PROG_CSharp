876221b8a9e537a2c3ff56be6ef1a6363388a577
Douglas Warner Jurado Peña <douglaswarner.jp@gmail.com>
2019-11-21 20:24:06 +0100

    Ejercicio 6 y 7

--- Relaciones/R5_Jurado_Douglas/Ejercicio6/Ejercicio6/Program.cs
+++ Relaciones/R5_Jurado_Douglas/Ejercicio6/Ejercicio6/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*----------------------------------------------------------------------
+ *   PROGRAMA: App_Ejercicio6
+ *	    AUTOR: Douglas Warner Jurado Peña
+ * 	    FECHA: 20/11/2019						VERSION: 1.0
+ * COMENTARIO: Inicializa a 0 una matriz de 20x20 de números enteros, luego la inicializa a número aleatorios y la muestra. Tambien copia la matriz a otra matriz.
+ *---------------------------------------------------------------------- */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,23 +19,27 @@ namespace Ejercicio6
             const int X = 20;
             const int Y = 20;
             int[,] array2D = new int[X, Y];
+            int[,] array2DDestino = new int[X, Y];
 
             Console.WriteLine("Esta aplicación iniciliza y muestra un array de 2 dimensiones. Y la copia a otra array.");
             Console.WriteLine();
-
+            Console.WriteLine("     Matriz 20x20 inicializada a 0, y la muestra.");
+            Console.WriteLine("--------------------------------------------------------------------------");
+            MostrarArray(array2D);
+            Console.WriteLine("\n   Inicializa la matriz 20x20 con números enteros, y la muestra.");
+            Console.WriteLine("--------------------------------------------------------------------------");
+            ArrayAlea(array2D);
+            MostrarArray(array2D);
+            Console.WriteLine("\n   Copia la matriz 20x20 a otra matriz 20x20, y la muestra.");
+            Console.WriteLine("--------------------------------------------------------------------------");
+            Console.WriteLine(" Array Destino");
+            MostrarArray(array2DDestino);
+            Console.WriteLine();
+            CopiarArray(array2D, array2DDestino);
+            Console.WriteLine(" Array copiado");
+            MostrarArray(array2DDestino);
 
             Console.ReadLine();
-        }
-
-        static void InicializarArray(int[,] array)
-        {
-            for (int i = 0; i < array.GetLength(0); i++)
-            {
-                for (int j = 0; j < array.GetLength(1); j++)
-                {
-                    array[i, j] = 0;
-                }
-            }
         }
 
         static void MostrarArray(int[,] array)
@@ -38,7 +48,7 @@ namespace Ejercicio6
             {
                 for (int j = 0; j < array.GetLength(1); j++)
                 {
-                    Console.Write(array[i,j]);
+                    Console.Write(array[i,j].ToString().PadLeft(3));
                 }
                 Console.WriteLine();
             }
---
+++ Relaciones/R5_Jurado_Douglas/Ejercicio7/Ejercicio7/Program.cs
@@ -0,0 +1,91 @@
+﻿/*----------------------------------------------------------------------
+ *   PROGRAMA: App_Ejercicio7
+ *	    AUTOR: Douglas Warner Jurado Peña
+ * 	    FECHA: 20/11/2019						VERSION: 1.0
+ * COMENTARIO: Dado una frase la encripta siguiendo el algoritmo de encriptación de cesar, tambien para desencriptar.
+ *---------------------------------------------------------------------- */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio7
+{
+    class Program
+    {
+        static string abecedario = "abcdefghijklmnñopqrstuvwxyz";
+
+        static void Main(string[] args)
+        {
+            string frase = string.Empty;
+            int desplazamiento = 3;
+            Console.WriteLine("Esta apliación encripta y desencripta con el algoritmo de encriptación de cesar.");
+            Console.Write("Escribe la frase a encriptar: ");
+            frase = Console.ReadLine();
+
+            Console.WriteLine(Encriptar(frase, desplazamiento));
+
+            Console.Write("Escriba la frase para desencriptar: ");
+            frase = Console.ReadLine();
+
+            Console.WriteLine(Desencriptar(frase, desplazamiento));
+
+            Console.ReadLine();
+        }
+
+        static string Encriptar(string frase, int desplazamiento)
+        {
+            int cantCaracter = frase.Length;
+            int posCaracter = 0;
+            string resultado = string.Empty;
+
+            for (int i = 0; i < cantCaracter; i++)
+            {
+                posCaracter = PosicionCaracter(frase[i]);
+
+                if (posCaracter != -1)
+                    resultado += abecedario[(posCaracter + desplazamiento) % abecedario.Length];
+                else
+                    resultado += frase[i];
+            }
+
+            return resultado.ToUpper();
+        }
+
+        static string Desencriptar(string frase, int desplazamiento)
+        {
+            int cantCaracter = frase.Length;
+            int posCaracter = 0;
+            int posicionResult = 0;
+            string resultado = string.Empty;
+
+            for (int i = 0; i < cantCaracter; i++)
+            {
+                posCaracter = PosicionCaracter(frase[i]);
+
+                if (posCaracter != -1)
+                {
+                    posicionResult = posCaracter - desplazamiento;
+                    if (posicionResult < 0)
+                        posicionResult += abecedario.Length;
+                    resultado += abecedario[posicionResult];
+                }
+                else
+                    resultado += frase[i];
+            }
+
+            return resultado.ToUpper();
+        }
+
+        static int PosicionCaracter(char letra)
+        {
+            for (int i = 0; i < abecedario.Length; i++)
+            {
+                if (letra == abecedario[i])
+                    return i;
+            }
+            return -1;
+        }
+    }
+}
