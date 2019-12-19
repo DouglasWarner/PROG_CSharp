efa17abf6c6b280d61b46675acf639186bb1f97c
Douglas Warner Jurado Peña <douglaswarner.jp@gmail>
2019-12-19 10:14:46 +0100

    Ejercicio R6

---
+++ Ejemplo_19-12/Ejemplo_19-12/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo_19_12
+{
+    public struct Ficha
+    {
+        // CUIIIIDAAAOOOOO CON PONERLO PUBLICO
+        public int id;
+        public string nombre;
+        public double estatura;
+        // Y muchos mas datos....
+
+        public Ficha(int i, string nom, double est)
+        {
+            id = i;
+            nombre = nom;
+            estatura = est;
+        }
+    
+    }
+
+    class Program
+    {
+        static Dictionary<int, Ficha> dic = new Dictionary<int, Ficha>();
+
+        static void Main(string[] args)
+        {
+            Ficha f1 = new Ficha(100, "pepito", 1.99);
+            Ficha f2 = new Ficha(101, "juanito", 1.20);
+            Ficha f3 = new Ficha(102, "joselito", 1.60);
+
+            if (Anadir(f1) && Anadir(f2) && Anadir(f3))
+            {
+                Console.WriteLine("Añadidos");
+            }
+            else
+                Console.WriteLine("Algo ocurrio con los datos");
+
+            Console.ReadLine();
+        }
+
+        static bool Anadir(Ficha f)
+        {
+            int clave = f.id;
+            if (dic.ContainsKey(clave))
+                return false;
+
+            dic.Add(clave, f);
+
+            return true;
+        }
+    }
+}
---
+++ Ejercicio7/Ejercicio7/Program.cs
@@ -0,0 +1,73 @@
+﻿/*----------------------------------------------------------------------
+ *   PROGRAMA: App_Ejercicio7
+ *	    AUTOR: Douglas Warner Jurado Peña
+ * 	    FECHA: 19/12/2019						VERSION: 1.0
+ * COMENTARIO: Función que valida un ISBN.
+ *---------------------------------------------------------------------- */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char separador = '-';
+            string isbn = string.Empty;
+
+            Console.WriteLine("     Esta aplicación valida el ISBN de un libro. Para salir introduce un [ * ].");
+            Console.WriteLine("".PadLeft(50,'-'));
+            do
+            {
+                Console.Write("\n   Dime el ISBN: ");
+                isbn = Console.ReadLine();
+
+                if (isbn == "*")
+                    return;
+
+                if (!ValidarISBN(QuitarGuiones(isbn, separador)))
+                    Console.WriteLine("\n\t Error: Algo ocurrio con el ISBN introducido.");
+                else
+                {
+                    Console.Write("El ISBN ");
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.Write(isbn);
+                    Console.ResetColor();
+                    Console.Write(" es correcto.");
+                }
+
+            } while (true);
+        }
+
+        static bool ValidarISBN(string isbn)
+        {
+            // ISBN de 0 a 9
+            // Digito de control de 0 a 9 o X
+            // MOD 11. Si el resultado es 10 el dc es X;
+
+            char dc = ' ';
+            if (isbn.Length < 0 && isbn.Length > 9)
+                return false;
+
+            return false;
+        }
+
+        static string QuitarGuiones(string isbn, char separador)
+        {
+            string[] split = isbn.Split(separador);
+            StringBuilder resultado = new StringBuilder();
+
+            foreach (string tmp in split)
+            {
+                resultado.Append(tmp);
+            }
+
+            return resultado.ToString();
+        }
+    }
+}
