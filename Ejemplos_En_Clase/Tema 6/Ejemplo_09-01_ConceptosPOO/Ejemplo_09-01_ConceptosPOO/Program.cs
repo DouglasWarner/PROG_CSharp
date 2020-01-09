901b88dfb08f30100fa143f6800271237766fa28
Douglas Warner Jurado Peña <douglaswarner.jp@gmail>
2020-01-09 10:13:44 +0100

    Ejemplo_ConceptosPOO ;

---
+++ Ejemplos_En_Clase/Tema 6/Ejemplo_09-01_ConceptosPOO/Ejemplo_09-01_ConceptosPOO/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo_09_01_ConceptosPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Vehiculo v1 = new Vehiculo("Bici", 10);
+
+            Coche c1 = new Coche("Seat", 90);
+
+            Vehiculo v2 = new Coche("Fiat",100);
+
+            v1.Andar("Vehiculo");
+
+            c1.Andar("Coche");
+
+            v2.Andar("Coche");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Ejemplos_En_Clase/Tema 6/Ejemplo_09-01_ConceptosPOO/Ejemplo_09-01_ConceptosPOO/Vehiculo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo_09_01_ConceptosPOO
+{
+    class Vehiculo
+    {
+        public string n;
+        int p;
+
+        public Vehiculo(string nom, int po)
+        {
+            n = nom;
+            p = po;
+        }
+
+        public void Andar(string nombre)
+        {
+            Console.WriteLine(nombre + " " + n + " esta andando " +p * 2);
+        }
+    }
+}
