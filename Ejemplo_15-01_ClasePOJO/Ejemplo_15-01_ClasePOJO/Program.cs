c68b2089b3ad3e7ef1c8db0a867886bf7f6efe38
Douglas Warner Jurado Peña <douglaswarner.jp@gmail>
2020-01-15 14:43:00 +0100

    Ejercicio 12

---
+++ Ejemplo_15-01_ClasePOJO/Ejemplo_15-01_ClasePOJO/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Douglas.Ejemplo_15_01_ClasePOJO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona yo = new Persona(100, "Risitas", "Jesus", DateTime.Now, 32600);
+
+            Console.WriteLine(yo);
+
+            Console.ReadLine();
+        }
+    }
+}
