a29a826f7dcf16f645d8fac3db05ebfa16c50625
Douglas Warner Jurado Peña <douglaswarner.jp@gmail>
2020-03-05 10:14:32 +0100

    Ejercicio 1 y 2 R(

---
+++ Relaciones/R8_Jurado_Douglas/Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,53 @@
+﻿/*----------------------------------------------------------------------
+ *   PROGRAMA: App_Ejercicio1
+ *	    AUTOR: Douglas Warner Jurado Peña
+ * 	    FECHA: 05/03/2020						VERSION: 1.0
+ * COMENTARIO: Devuelve información sobre el fichero seleccionado.
+ *---------------------------------------------------------------------- */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//-----------------------------
+using System.IO;
+using System.Windows.Forms;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            string ruta = string.Empty;
+            OpenFileDialog fichero = new OpenFileDialog();
+
+            if (fichero.ShowDialog() == DialogResult.OK)
+            {
+                ruta = fichero.FileName;
+                InfoFicheroActual(ruta);
+            }
+
+            Console.ReadLine();
+        }
+
+        static void InfoFicheroActual(string ruta)
+        {
+            Console.WriteLine("                 INFORMACION DEL FICHERO     ");
+            Console.WriteLine("---------------------------------------------------------------------------------------\n");
+            Console.WriteLine("            Ruta y fichero original: {0}", ruta);
+            Console.WriteLine("                          Extensión: {0}", Path.GetExtension(ruta));
+            Console.WriteLine("        Nombre completo del fichero: {0}", Path.GetFileName(ruta));
+            Console.WriteLine("   Nombre del fichero sin extensión: {0}", Path.GetFileNameWithoutExtension(ruta));
+            Console.WriteLine("     Obtener directorio del fichero: {0}", Path.GetDirectoryName(ruta));
+            Console.WriteLine("                    Directorio raíz: {0}", Path.GetPathRoot(ruta));
+            Console.WriteLine("    Determina si contiene extension: {0}", Path.HasExtension(ruta));
+            Console.WriteLine("     Determina si contiene una raíz: {0}", Path.IsPathRooted(ruta));
+            Console.WriteLine("              Atributos del fichero: {0}", File.GetAttributes(ruta));
+            Console.WriteLine("   Fecha de la creación del fichero: {0}", File.GetCreationTime(ruta));
+            Console.WriteLine("Fecha del último acceso del fichero: {0}", File.GetLastAccessTime(ruta));
+            Console.WriteLine("    Fecha de la última modificación: {0}", File.GetLastWriteTime(ruta));
+        }
+    }
+}
---
+++ Relaciones/R8_Jurado_Douglas/Ejercicio2/Ejercicio2/Program.cs
@@ -0,0 +1,55 @@
+﻿/*----------------------------------------------------------------------
+ *   PROGRAMA: App_Ejercicio2
+ *	    AUTOR: Douglas Warner Jurado Peña
+ * 	    FECHA: 05/03/2020						VERSION: 1.0
+ * COMENTARIO: Devuelve información sobre el directorio seleccionado.
+ *---------------------------------------------------------------------- */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//-----------------------------------
+using System.IO;
+using System.Windows.Forms;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            string directorio = string.Empty;
+            OpenFileDialog ventana = new OpenFileDialog();
+            FolderBrowserDialog d = new FolderBrowserDialog();
+
+            if (d.ShowDialog() == DialogResult.OK)
+            {
+                directorio = d.SelectedPath;
+                InfoDirectorio(directorio);
+            }
+
+            Console.ReadLine();
+        }
+
+        static void InfoDirectorio(string directorio)
+        {
+            DirectoryInfo dir = new DirectoryInfo(directorio);
+
+            Console.WriteLine("         INFORMACIÓN DIRECTORIO  ");
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine("           Nombre completo del directorio: {0}", dir.FullName);
+            Console.WriteLine("                 Atributos del directorio: {0}", dir.Attributes);
+            Console.WriteLine("      Fecha de la creacion del directorio: {0}", dir.CreationTime);
+            Console.WriteLine(" Cantidad de directorios en el directorio: {0}", dir.EnumerateDirectories().Count());
+            Console.WriteLine("    Cantidad de ficheros en el directorio: {0}", dir.EnumerateFiles().Count());
+            Console.WriteLine("                                Extension: {0}", dir.Extension);
+            Console.WriteLine("    Fecha del último acceso al directorio: {0}", dir.LastAccessTime);
+            Console.WriteLine("          Fecha de la última modificación: {0}", dir.LastWriteTime);
+            Console.WriteLine("                                   Nombre: {0}", dir.Name);
+            Console.WriteLine("                      El directorio padre: {0}", dir.Parent);
+            Console.WriteLine("                   La raíz del directorio: {0}", dir.Root);
+        }
+    }
+}
